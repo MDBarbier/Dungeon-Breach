6c01a87037fb755f6d2738cc4b7864990d88c714
mdbarbier <matthewdavidbarbier@gmail.com>
2020-10-04 16:21:10 +0100

    Added dice roller script

---
+++ src/mvp-build/2020.02.0b4/mvp_v1/Assets/Scripts/DiceRoller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assets.Scripts
+{
+    class DiceRoller
+    {
+        private Random random;
+
+        public DiceRoller()
+        {
+            random = new Random();
+        }
+
+        public int RollDie(int sides) => random.Next(1, sides);        
+
+        public int[] RollDie(int sides, int number)
+        {
+            var array = new int[number];
+            for (int i = 0; i < number; i++)
+            {
+                array[i] = random.Next(1, sides);
+            }
+            return array;
+        }
+    }
+}
